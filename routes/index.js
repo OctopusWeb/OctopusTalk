71476e1265b38af7b8eec34741a8fdbf01f40811
80883080@qq.com <80883080@qq.com>
2016-11-23 11:58:56 +0800

    ceshi3

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res, next) {
   res.render('index');
 });
-
-io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-	    io.emit('chat message', msg);
-	});
-});
+//
+//io.on('connection', function(socket){
+//	socket.on('chat message', function(msg){
+//	    io.emit('chat message', msg);
+//	});
+//});
 
 module.exports = app;
 //
